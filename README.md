4b9fe329e05b9248ad64c94da971a3b80d9e33c7
kyler <100742516+ky-ler@users.noreply.github.com>
2023-06-24 02:53:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# StayAwake
+
+- Prevent your display from going to sleep
+
+- Created for Windows
+
+## Instructions
+
+- Right click the icon in your system tray to view options
+
+App icon is from [Gartoon Redux](https://github.com/daemonblade/gartoon-redux)
---
+++ StayAwake/StayAwakeContext.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StayAwake;
+
+public class StayAwakeContext : ApplicationContext
+{
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+    [Flags]
+    private enum EXECUTION_STATE : uint
+    {
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002
+    }
+
+    private readonly NotifyIcon _trayIcon;
+    private bool _isActive;
+
+    public StayAwakeContext()
+    {
+        _trayIcon = new NotifyIcon
+        {
+            Icon = new Icon("Resources/gnome_eyes.ico"),
+            ContextMenuStrip = new ContextMenuStrip
+            {
+                Items =
+                {
+                    new ToolStripMenuItem("Active", null, KeepAwake),
+                    new ToolStripMenuItem("About", null, ShowAbout),
+                    new ToolStripMenuItem("Exit", null, Exit)
+                }
+            },
+            Text = "StayAwake - Inactive",
+            Visible = true
+        };
+    }
+
+    private void Exit(object? sender, EventArgs e)
+    {
+        _trayIcon.Visible = false;
+        // Reset to default ThreadExecutionState before closing
+        SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        Application.Exit();
+    }
+
+    private void KeepAwake(object? sender, EventArgs e)
+    {
+        if (sender != null)
+        {
+            var buttonText = (ToolStripItem)sender;
+
+            _trayIcon.ContextMenuStrip!.Items[0].Text = buttonText.Text.Equals("Active") ? "Deactivate" : "Active";
+            _trayIcon.Text = _trayIcon.Text.Equals("StayAwake - Inactive")
+                ? "StayAwake - Active"
+                : "StayAwake - Inactive";
+        }
+
+        _isActive = !_isActive;
+
+        SetThreadExecutionState(_isActive ? EXECUTION_STATE.ES_DISPLAY_REQUIRED : EXECUTION_STATE.ES_CONTINUOUS);
+    }
+
+    private static void ShowAbout(object? sender, EventArgs e)
+    {
+        MessageBox.Show(
+            "Prevent your display from automatically sleeping." +
+            "\nCreated by Kyler" +
+            "\nGitHub: https://github.com/ky-ler/StayAwake" +
+            "\nApp Icon: https://github.com/daemonblade/gartoon-redux",
+            "StayAwake",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+
+    }
+}
